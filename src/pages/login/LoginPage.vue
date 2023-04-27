7972015fe25da2be5f5d07bc7898c0802405c23c
mavyfaby <maverickfabroa@gmail.com>
2023-04-27 09:50:19 +0800

    add login chu chu

--- src/pages/login/LoginPage.vue
+++ src/pages/login/LoginPage.vue
@@ -4,16 +4,16 @@
       <VCard :loading="isLoading" class="mt-16 w-full sm:w-3/4 md:w-3/5 lg:w-1/2 xl:w-1/3">
         <h3 class="title">Login</h3>
         <div class="flex flex-col">
-          <!-- Username -->
+          <!-- Email -->
           <md-filled-text-field
-            v-model="user"
+            v-model="email"
             class="mt-4"
-            label="Username"
+            label="Email"
             @keydown.enter="login"
             :disabled="isDisabled"
-            placeholder="Enter username"
+            placeholder="Enter email"
           >
-            <md-icon slot="leadingicon">person</md-icon>
+            <md-icon slot="leadingicon">email</md-icon>
           </md-filled-text-field> 
 
           <!-- Password -->
@@ -47,18 +47,58 @@ import { useRouter } from "vue-router";
 import { useStore } from "~/store";
 
 import VCard from "~/components/VCard.vue";
+import showToast from "~/utils/toast";
+import { TYPE } from "vue-toastification";
+import makeRequest, { Endpoints } from "~/network/request";
 
-const user = ref("");
+const email = ref("");
 const pass = ref("");
 const isDisabled = ref(false);
 const isLoading = ref(false);
 const loginLabel = ref("Login");
 
-const store = useStore();
 const router = useRouter();
 
 function login() {
+  if (!email.value || !pass.value) {
+    showToast(TYPE.ERROR, "Please fill all fields");
+    return;
+  }
 
+  isLoading.value = true;
+
+  makeRequest("POST", Endpoints.Login, {
+    email: email.value,
+    password: pass.value,
+  }, (err, response) => {
+    if (err) {
+      showToast(TYPE.ERROR, "Something went wrong");
+      isLoading.value = false;
+      return;
+    }
+
+    if (response.error) {
+      showToast(TYPE.ERROR, response.error);
+      isLoading.value = false;
+      return;
+    }
+
+    isLoading.value = false;
+
+    const { type, id } = response;
+
+    switch (type) {
+      case 1:
+        router.push({ name: "Admin" });
+        break;
+      case 2:
+        router.push({ name: "Techlead" });
+        break;
+      case 3:
+        router.push({ name: "Employee" });
+        break;
+    }
+  });
 }
 </script>
 
